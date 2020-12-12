932cd55369928c9c9ca4780650d64a573afa57f0
Andrew Ogle <35720141+andrewogle@users.noreply.github.com>
2020-12-12 10:32:38 -0600

    init

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Dingers are the best</Text>
       <StatusBar style="auto" />
     </View>
   );
